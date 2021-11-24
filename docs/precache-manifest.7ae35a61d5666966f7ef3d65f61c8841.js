68af41db17a6657cf3ff8d44b3e86e813f28f177
Bastian Mardones <57372582+bamardto@users.noreply.github.com>
2021-11-24 16:22:37 -0300

    second commit

---
+++ docs/precache-manifest.7ae35a61d5666966f7ef3d65f61c8841.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fb7709ab25df8948a9be7ff26e8971",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc9367b2ea557cae323",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "8c639a6bff16b54b9f77",
+    "url": "/static/js/2.629c55af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.629c55af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc9367b2ea557cae323",
+    "url": "/static/js/main.098a0ff7.chunk.js"
+  },
+  {
+    "revision": "5bba4b9ed69c6568a70e",
+    "url": "/static/js/runtime-main.95118175.js"
+  }
+]);
